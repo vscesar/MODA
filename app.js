c73e3711ef92aa5f2f0a70eb64bf905f42644811
vscesar <136080864+vscesar@users.noreply.github.com>
2023-11-10 15:46:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+
+const boton1 = document.getElementById('boton1');
+const boton2 = document.getElementById('boton2');
+
+boton1.addEventListener('click', () => {
+  alert('Hiciste clic en el botón 1!'); 
+});
+
+boton2.addEventListener('click', () => {
+  alert('Hiciste clic en el botón 2!');
+});
